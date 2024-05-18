f5d4730c7daee3c74d28dd666b22581a5628a1af
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-18 08:15:19 +0600

    basic setup and route impliment

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <details className="dropdown">
+          <summary className="m-1 btn">open or close</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
+        </details>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Shared/Navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+
+
+const NavBar = () => {
+    return (
+        <div>
+            <h3>This is navbar</h3>
+        </div>
+    );
+};
+
+export default NavBar;
