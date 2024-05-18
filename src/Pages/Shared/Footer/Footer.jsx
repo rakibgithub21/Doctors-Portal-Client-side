fb74f3229a1eef9eafb92f25e1f14e93054ed119
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-18 13:21:50 +0600

    add a footer

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,36 @@
 
-
+import footer from '../../../assets/images/footer.png';
 const Footer = () => {
     return (
-        <div>
-            <h3>This is Footer</h3>
-        </div>
+        <footer style={{backgroundImage:`url(${footer})`,backgroundSize:'cover'}} >
+            <div className="footer p-10 ">
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </div>
+            <div className="footer mt-5 footer-center p-4 bg-base-300 text-base-content">
+                <aside>
+                    <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+                </aside>
+            </div>
+        </footer>
     );
 };
 
