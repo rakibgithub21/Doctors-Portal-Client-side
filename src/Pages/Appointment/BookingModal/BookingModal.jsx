f5fbcfd9257d82b6272308f9f004a7a3c9a9b2ea
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-05-20 18:59:46 +0600

    get data in form tage

--- src/Pages/Appointment/BookingModal/BookingModal.jsx
+++ src/Pages/Appointment/BookingModal/BookingModal.jsx
@@ -7,7 +7,18 @@ const BookingModal = ({ treatment, selectedDate }) => {
     
     const { namem,slots } = treatment; //treatment is appointment options just different name
 
-    const date = format(selectedDate,'PP')
+    const date = format(selectedDate, 'PP')
+
+    const handleBooking = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const slot = form.slot.value;
+        const phone = form.phone.value;
+        console.log(name,email,slot,phone,date);
+
+    }
     return (
         <>
             {/* You can open the modal using document.getElementById('ID').showModal() method */}
@@ -15,9 +26,9 @@ const BookingModal = ({ treatment, selectedDate }) => {
             <div className="modal" role="dialog">
                 <div className="modal-box">
                     <h3 className="font-bold text-lg my-4">{name }</h3>
-                    <form className="space-y-3">
+                    <form onSubmit={handleBooking} className="space-y-3">
                         <input type="text" readOnly value={date} className="input input-bordered w-full " />
-                        <select className="select select-bordered w-full ">
+                        <select name="slot" className="select select-bordered w-full ">
                             
                             {
                                 slots.map(slot => <option
@@ -28,9 +39,9 @@ const BookingModal = ({ treatment, selectedDate }) => {
                             {/* <option>Han Solo</option>
                             <option>Greedo</option> */}
                         </select>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full " />
+                        <input name="name" type="text" placeholder="Your Name" className="input input-bordered w-full " />
+                        <input name="email" type="text" placeholder="Your Email" className="input input-bordered w-full " />
+                        <input name="phone" type="text" placeholder="Your Phone Number" className="input input-bordered w-full " />
                         <input type="submit" className="btn w-full btn-secondary text-white" value="Submit" />
                    </form>
                     <div className="modal-action">
